66af62c76995304427a8517ebcb0dd858a9a24cb
CAMILA ROMERO <camilaar@byui.edu>
2022-05-14 19:42:24 -0400

    commit

--- www/phpmotors/accounts/index.php
+++ www/phpmotors/accounts/index.php
@@ -10,7 +10,7 @@ $classifications = getClassifications();
 $navList = '<ul>';
 $navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
 foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+    $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
 }
 $navList .= '</ul>';
 
@@ -31,4 +31,5 @@ $action = filter_input(INPUT_POST, 'action');
 
     default:
         include '../view/home.php';
+        break;
  }
