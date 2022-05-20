0f9afe75df0c3687be0d8594a5661bac29f9aa43
CAMILA ROMERO <camilaar@byui.edu>
2022-05-20 11:32:24 -0400

    commit

--- www/phpmotors/accounts/index.php
+++ www/phpmotors/accounts/index.php
@@ -1,35 +1,56 @@
 <?php
-// Get the database connection file
-require_once '../library/connections.php';
-// Get the PHP Motors model for use as needed
-require_once '../model/main-model.php';
 
-// Get the array of classifications
-$classifications = getClassifications();
 
-$navList = '<ul>';
-$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
-foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
-}
-$navList .= '</ul>';
+    require_once '../library/connections.php';
+    require_once '../model/main-model.php';
+    require_once '../model/accounts-model.php';
 
-$action = filter_input(INPUT_POST, 'action');
- if ($action == NULL){
-  $action = filter_input(INPUT_GET, 'action');
- }
- switch ($action) {
-    case 'template':
-        include "../view/template.php";
-        break;
-    case 'login':
-        include "../view/login.php";
-        break;
-    case 'register':
-        include "../view/registration.php";
-        break;
+    $classifications = getClassifications();
 
-    default:
-        include '../view/home.php';
+    // Build a navigation bar using the $classifications array
+    $navList = "<ul id='navigation'>";
+    $navList .= "<li><a href='/phpmotors/accounts/index.php' title='View the PHP Motors home page'>Home</a></li>";
+    foreach ($classifications as $classification) {
+        $navList .="<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+    }
+    $navList .='</ul>';
+
+    $action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+    }
+    
+    switch ($action) {
+        case 'register':
+            $clientFirstname = filter_input(INPUT_POST, 'clientFirstname');
+            $clientLastname = filter_input(INPUT_POST, 'clientLastname');
+            $clientEmail = filter_input(INPUT_POST, 'clientEmail');
+            $clientPassword = filter_input(INPUT_POST, 'clientPassword');
+            if(empty($clientFirstname) || empty($clientLastname) || empty($clientEmail) || empty($clientPassword)) {
+                $message = '<p>Please provide information for all empty form fields.</p>';
+                include '../view/registration.php';
+                exit;
+            }
+            $regOutcome = regClient($clientFirstname, $clientLastname, $clientEmail, $clientPassword);
+            if($regOutcome === 1){
+                $message = "<p>Thanks for registering $clientFirstname. Please use your email and password to login.</p>";
+                include '../view/login.php';
+                exit;
+            } else {
+                $message = "<p>Sorry $clientFirstname, but the registration failed. Please try again.</p>";
+                include '../view/registration.php';
+                exit;
+            }
+            break;
+        case 'login':
+            include "../view/login.php";
         break;
- }
+        case 'register-page':
+            include "../view/registration.php";
+            break;  
+        default:
+            include '../view/home.php';
+            break;
+    }
+
+?>
--- www/phpmotors/view/registration.php
+++ www/phpmotors/view/registration.php
@@ -1,30 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="phpmotors" content="CSE 340">
-    <meta name="author" content="Laura Salcedo Maso">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href= "/phpmotors/css/style.css" />
-    <title>Account registration | PHP Motors </title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home | PHP Motors</title>    
+        <link rel="stylesheet" type="text/css" href= "/phpmotors/css/style.css" />
+    </head>
+    <body>
+        <div id="body">
+            <header>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/navigation.php'; ?>
-        <div class="registration">
-            <h1>Register</h1>
-            <label for="firstname">Email</label><br>
-            <input type="text" name="first_name" id="firstname"><br>
-            <label for="lastname">Last Name</label><br>
-            <input type="text" name="last_name" id="lastname"><br>
-            <label for="email">Email</label><br>
-            <input type="text" name="user_email" id="email"><br>
-            <p>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</p>
-            <label for="password">Password</label><br>
-            <input type="text" name="user_password" id="password"><br>
-            <button class="showPassword">Show Passowrd</button><br>
-            <button class="submit">Register</button><br>
-        </div>
+</header>
+            
+            <main>
+                <nav>
+<!-- <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/navigation.php';?> -->
+<?php echo $navList;?>
+</nav>
+                <h1>Register</h1>
 
-    <hr />
-
+                <?php 
+                    if (isset($message)) {
+                        echo $message;
+                    }
+                ?>
+                <form action="/phpmotors/accounts/index.php" method="post">
+                    <label for="clientFirstname">First Name: </label>
+                    <input type="text" name="clientFirstname" id="clientFirstname"><br>
+                    <label for="clientLastname">Last Name: </label>
+                    <input type="text" name="clientLastname" id="clientLastname"><br>
+                    <label for="clientEmail">Email Address: </label>
+                    <input type="text" name="clientEmail" id="clientEmail"><br> 
+                    <p>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</p>
+                    <label for="clientPassword">Password: </label>
+                    <input type="password" name="clientPassword" id="clientPassword"><br>
+                    <button class="submit">Register</button><br>
+                    <input type="hidden" name="action" value="register">
+                </form>
+            
+            </main>
+            <hr />
+            <footer>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
+
+</footer>
+        </div>
+    </body>
+</html>
